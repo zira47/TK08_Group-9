93065cda42812ec126a54f99e857cdd8b32c0dfb
zira47 <nadziradaviana7@gmail.com>
2026-05-01 22:46:47 +0700

    menambahkan pilihan operasi matematika add dan sub

--- calculator.c
+++ calculator.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+int main() {
+    float num1, num2, result;
+    int choice;
 
-int main(void) {
+    printf("=== Simple Calculator ===\n");
+    printf("1. Add (Addition)\n");
+    printf("2. Sub (Subtraction)\n");
+    
+    printf("Pilih operasi (1-2): ");
+    scanf("%d", &choice);
+
+    printf("Masukkan dua angka (pisahkan dengan spasi): ");
+    scanf("%f %f", &num1, &num2);
+
+    
 
     return 0;
 }
